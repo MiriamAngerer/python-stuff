fc721aaadb5b77e777c4c7d80e0024cb46cbb2d2
Miriam Angerer <miriam-angerer@gmx.at>
2016-12-27 17:59:10 +0100

    first commit

--- csi-forensics.py
+++ csi-forensics.py
@@ -1,69 +1,81 @@
 dna = open("dna.txt").read()
 
-suspect = {}
+match = {}
 
 # hair color
-if dna.find('CCAGCAATCGC'):
-    suspect["hair_color"] = "black"
+if dna.find("CCAGCAATCGC"):
+    match["hair color"] = "black"
 
-elif dna.find('GCCAGTGCCG'):
-    suspect["hair_color"] = "brown"
+elif dna.find("GCCAGTGCCG"):
+    match["hair color"] = "brown"
 
-elif dna.find('TTAGCTATCGC'):
-    suspect["hair_color"] = "blonde"
+elif dna.find("TTAGCTATCGC"):
+    suspect["hair color"] = "blonde"
 
 # Facial shape
-if dna.find('GCCACGG'):
-    suspect["facial_shape"] = "square"
+if dna.find("GCCACGG"):
+    match["facial shape"] = "square"
 
-elif dna.find('ACCACAA'):
-    suspect["facial_shape"]  = "round"
+elif dna.find("ACCACAA"):
+    match["facial shape"]  = "round"
 
-elif dna.find('AGGCCTCA'):
-    suspect["facial_shape"]  = "oval"
+elif dna.find("AGGCCTCA"):
+    match["facial shape"]  = "oval"
 
 # Eye color
-if dna.find('TTGTGGTGGC'):
-    suspect["eye_color"] = "blue"
+if dna.find("TTGTGGTGGC"):
+    match["eye color"] = "blue"
 
-elif dna.find('GGGAGGTGGC'):
-    suspect["eye_color"] = "green"
+elif dna.find("GGGAGGTGGC"):
+    match["eye color"] = "green"
 
-elif dna.find('AAGTAGTGAC'):
-    suspect["eye_color"] = "brown"
+elif dna.find("AAGTAGTGAC"):
+    match["eye color"] = "brown"
 
 # Gender
-if dna.find('TGAAGGACCTTC'):
-    suspect["gender"] = "female"
+if dna.find("TGAAGGACCTTC"):
+    match["gender"] = "female"
 
-elif dna.find('TGCAGGAACTTC'):
-    suspect["gender"] = "male"
+elif dna.find("TGCAGGAACTTC"):
+    match["gender"] = "male"
 
-# Skin Color
-if dna.find('TGAAGGACCTTC'):
-    suspect["skin_color"] = "white"
+# Further characteristic
+if dna.find("AAAACCTCA"):
+    match["further characteristic"] = "white"
 
-elif dna.find('CGACTACAG'):
-    suspect["skin_color"] = "black"
+elif dna.find("CGACTACAG"):
+    match["further characteristic"] = "black"
 
-Eva = {"hair_color" : "blonde", "facial_shape" : "oval", "eye_color" : "blue", "gender" : "female", "skin_color" : "white"}
-Larisa = {"hair_color" : "brown", "facial_shape" : "oval", "eye_color" : "brown", "gender" : "female", "skin_color" : "white"}
-Matej = {"hair_color" : "black", "facial_shape" : "oval", "eye_color" : "blue", "gender" : "male", "skin_color" : "white"}
-Miha = {"hair_color" : "brown", "facial_shape" : "square", "eye_color" : "green", "gender" : "male", "skin_color" : "white"}
+elif dna.find("CGCGGGCCG"):
+    match["further characteristic"] = "asian"
 
-if Eva == suspect:
+
+
+Eva = {"hair color" : "blonde", "facial_shape" : "oval", "eye color" : "blue", "gender" : "female", "further characteristic" : "white"}
+Larisa = {"hair color" : "brown", "facial_shape" : "oval", "eye color" : "brown", "gender" : "female", "further characteristic" : "white"}
+Matej = {"hair color" : "black", "facial_shape" : "oval", "eye color" : "blue", "gender" : "male", " further characteristic" : "white"}
+Miha = {"hair color" : "brown", "facial_shape" : "square", "eye color" : "green", "gender" : "male", "further characteristic" : "white"}
+
+if Eva == match:
     print "Eva ate all the ice cream."
 
-elif Larisa == suspect:
+elif Larisa == match:
     print "Larisa ate all the ice cream."
 
-elif Matej == suspect:
+elif Matej == match:
     print "Matej ate all the ice cream."
 
-elif Miha == suspect:
+elif Miha == match:
     print "Miha ate all the ice cream."
 
 else:
-    print "None of the suspects ate the ice cream. The ice cream thief is still out there. Probably eating ice cream."
-
+    print "None of the suspects ate the ice cream. The ice cream thief is still out there. Probably looking for more ice cream right now."
+    print "---------------------------------------------------------------------"
+    print "We are looking for a person with following characteristics:"
+    for i in match:
+        characteristics = match.items()
+        for k, v in characteristics:
+            print k, ":", v
+        break
+    print "---------------------------------------------------------------------"
 
